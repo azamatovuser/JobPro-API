29cd006742b70e352408d419bef208a70e294ebb
aazamatoff <jonh1234554321@gmail.com>
2023-04-28 15:05:51 +0500

    updates in models

--- account/models.py
+++ account/models.py
@@ -2,17 +2,42 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Country(models.Model):
+    title = models.CharField(max_length=221)
+
+
+class City(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    title = models.CharField(max_length=221)
+
+
+class Company(models.Model):
+    title = models.CharField(max_length=221)
+
+    def __str__(self):
+        return self.title
+
+
 class Account(models.Model):
-    POSITION = (
-        (0, 'Employer'),
-        (0, 'Worker'),
+    TYPE = (
+        (0, 'HR'),
+        (1, 'Candidate'),
+        (2, 'Staff'),
     )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='account_avatar/')
-    position = models.IntegerField(choices=POSITION)
-    location = models.CharField(max_length=221)
+    type = models.IntegerField(choices=TYPE)
     bio = models.TextField()
-    company = models.CharField(max_length=221)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
+
+
+class WorkHistory(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    location = models.ForeignKey(City, on_delete=models.CASCADE)
+    start_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField(auto_now_add=True)
+    is_current = models.BooleanField(default=True)
--- job/models.py
+++ job/models.py
@@ -16,23 +16,37 @@ class Category(models.Model):
         return self.title
 
 
+class Type(models.Model):
+    title = models.CharField(max_length=221)
+
+    def __str__(self):
+        return self.title
+
+
+class Company(models.Model):
+    title = models.CharField(max_length=221)
+
+    def __str__(self):
+        return self.title
+
+
 class Job(models.Model):
-    TYPES = (
-        (0, "Full time"),
-        (1, "Part time"),
-        (2, "Freelance"),
-        (3, "Internship"),
-        (4, "Temporary"),
-    )
     author = models.ForeignKey(Account, on_delete=models.CASCADE)
     title = models.CharField(max_length=221)
     description = models.TextField()
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
     deadline = models.CharField(max_length=221)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
     salary = models.DecimalField(decimal_places=3, max_digits=4, null=True, blank=True)
-    types = models.IntegerField(choices=TYPES)
+    types = models.ManyToManyField(Type)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
+class Wishlist(models.Model):
+    author = models.ForeignKey(Account, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
